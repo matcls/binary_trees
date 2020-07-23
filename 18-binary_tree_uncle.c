cbb636981ed3e9bcc98531de2a71f934ec2a3f48
matcls <manueltorresvesga@gmail.com>
2020-07-23 11:38:05 -0500

    another fix for task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - sibling of a node
+ * binary_tree_uncle - incle of a node
  * @node: pointer input
- * Return: pointer sibling
+ * Return: pointer uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
